202ee475d7436519f55e6fae5c8c40417016f829
Leo <146775205+LeoAboard@users.noreply.github.com>
2024-03-15 14:36:42 -0400

    Add files via upload

---
+++ Radix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct Tree{
+    char valor[4];
+    struct Tree *left;
+    struct Tree *right;
+} tree;
+
+
+
+void insert(int x, char valor[]){
+    for(){
+
+    }
+
+    return;
+}
+
+
